cdb9df70a88734f91ba699ead30da107525b519e
MangoProgrammer2024 <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-07 13:23:26 -0500

    Update i_eBrush.h

--- Includes/i_eBrush.h
+++ Includes/i_eBrush.h
@@ -22,8 +22,19 @@ public:
 static int i_eBrushNumberId = 0;
 iEBoolean i_eBrushCreated();
 
+enum i_eBrushMode{
+    i_eFreeMode = 0,
+    i_eLockMode = 1,
+    i_eEpairMode = 2
+};
+
+iEBoolean BrushDeleted(i_eBrush);
+
+i_eBrush * qt_ieBrushOwner, * qt_ieBrushPrevious, * qt_ieBrushNext;
+
 virtual void Create_Brush(i_eBrush& iebrush, iEBoolean i_eBrushCreatedTrue, static char * i_eSpawn, static int i_eBrushSpawnflags);
 virtual void BrushEpair(i_eBrush * ieglobal, const char * brushname, const i_ePreference * i_eKey, const i_ePreference * i_eValue);
+void Delete_Brush(i_eBrush * brushCurrent, unsigned int brushFaces);
 
  class i_ebrushtable{
   public:
