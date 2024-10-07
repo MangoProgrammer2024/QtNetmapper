8dc70174a7cd404efa64e4803cfb04edd8a0280c
MangoProgrammer2024 <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-07 13:17:31 -0500

    Update and rename I_EBrush.h to i_eBrush.h

--- Includes/i_eBrush.h
+++ Includes/i_eBrush.h
@@ -23,6 +23,7 @@ static int i_eBrushNumberId = 0;
 iEBoolean i_eBrushCreated();
 
 virtual void Create_Brush(i_eBrush& iebrush, iEBoolean i_eBrushCreatedTrue, static char * i_eSpawn, static int i_eBrushSpawnflags);
+virtual void BrushEpair(i_eBrush * ieglobal, const char * brushname, const i_ePreference * i_eKey, const i_ePreference * i_eValue);
 
  class i_ebrushtable{
   public:
