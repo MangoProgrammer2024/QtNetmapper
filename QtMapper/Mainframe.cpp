2ff315694377aa80f53309203a23aed9ebc7b6cb
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-08 09:37:21 -0500

    Update Mainframe.cpp

--- QtMapper/Mainframe.cpp
+++ QtMapper/Mainframe.cpp
@@ -1,5 +1,9 @@
-#include <QtWidget>
+#include <QAction>
+#include <QWidget>
 
 int main(int args, char**argv){
-    QtWidget * iQWindow;
+    QWidget * iQWindow;
+
+QAction * construct_main_frame(QWidget * iQWindow, const char * title, static int iQFlags);
+    
 };
