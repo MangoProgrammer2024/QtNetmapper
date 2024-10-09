1447f8b36debd2f8b78a441ce72a9caee58f2d06
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-09 07:37:39 -0500

    Create i_eSystem.h

---
+++ Includes/i_eSystem.h
@@ -0,0 +1,24 @@
+/*
+         *This file is part of QtNetmapper*
+                *File: i_eSystem.h*
+*/
+#pragma once
+
+#ifndef I_ESYSTEM_H
+#define I_ESYSTEM_H
+
+#endif
+
+typedef char Sys_charf;
+
+const Sys_charf * charprintf(Sys_charf*)const;
+
+typedef class Sys_callf{
+public:
+
+ Sys_callf();
+ virtual ~Sys_callf();
+
+extern virtual void Sys_printf(const Sys_charf * text, ...);
+
+};
