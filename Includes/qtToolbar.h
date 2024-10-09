e1ae5632dcd286d67cda911ca9a46a7f3235618c
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-09 10:07:41 -0500

    Update qtToolbar.h

--- Includes/qtToolbar.h
+++ Includes/qtToolbar.h
@@ -15,7 +15,12 @@ public:
 
 const char * i_eToolbarName;
 QWidget * i_eQWindowToolbarFrame;//holder frame for toolbar
-QAction * construct_toolbar(QToolbar * i_eToolbar, int x, int y, bool i_eLoadButtons);
+QAction * construct_toolbar(QToolbar * i_eToolbar, int _x, int _y, bool i_eLoadButtons);
+void iToolbar(QToolbar * i_eToolbar);
+
+i_eToolbar * Alloc_ieToolbar(){
+   i_eToolbar * toolbar = (i_eToolbar*)malloc(sizeof(i_eToolbar));
+};
 
 
 };
