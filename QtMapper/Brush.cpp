5a259a03e3c989e3d7a93a32d764e3ed6f01a6a3
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-09 17:14:02 -0500

    Update Brush.cpp

--- QtMapper/Brush.cpp
+++ QtMapper/Brush.cpp
@@ -1,6 +1,7 @@
 /*
         *This file is part of QtNetmapper*
                *File: Brush.cpp*
+*Brush.cpp is not for printing a 3D brush it is only for handling brush information*
 */
 
 #include "i_eBrush.h"
@@ -20,4 +21,9 @@ const char * Brush_Global(i_eBrush * brush){
 i_eBrush * Alloc_Brush(){
   i_eBrush * brush =(i_eBrush*)malloc(sizeof(i_eBrush));
   return brush;
+};
+
+i_eBrush * Brush_Epair(){
+ i_eBrush * brush_e = (i_eBrush*)const(char * iKey, char * eValue(i_eBrush));
+ return brush_e;  
 };
