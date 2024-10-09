0dd7ab43dfa8d774346611e6a29a4252465974a1
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-09 08:34:10 -0500

    Update i_eShader.h

--- Includes/i_eShader.h
+++ Includes/i_eShader.h
@@ -2,3 +2,33 @@
       *This file belongs to QtNetmapper*
           *File: i_eShader.h*
 */
+
+#ifndef I_ESHADER_H
+#define I_ESHADER_h
+
+typedef struct i_eShader{
+
+//shader name return to i_eShader structure
+const char * i_eShadername(i_eShader *) const;
+
+/*shader file type*/
+enum i_eSFType{
+     i_ePng = 0,
+     i_eJpg = 1,
+     i_eTga = 2,
+     i_eDds = 3,
+     i_eBmp = 4//not recommended
+}
+
+static int iPixelShaderBits = 32;//bits for pixel shader
+float iPixelsResolution = 64;//resolution of pixels for shader
+
+double pR;//r
+double pG;//b
+double pB;//g
+
+void i_eShaderFile(i_eShader * shaderftype, const char * iFilename, static int pBits, float pResolution);
+
+};
+
+#endif
