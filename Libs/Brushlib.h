f166d9078984083bca3f104064fe3dfea7fa1037
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-17 12:16:53 -0500

    Update Brushlib.h

--- Libs/Brushlib.h
+++ Libs/Brushlib.h
@@ -23,4 +23,25 @@ enum brushtype{
  THollowBrush = 2
 };
 
+brush_t * alloc();
+
+/*
+* brush functions
+*/
+void brush_free(brush_t * b, bool free);
+void brush_epair(brush_t * b, const char * key, const char * value);
+void brush_link(brush_t * b, *owner, *child, const char * brush_name);
+void brush_lock(brush_t * block, bool lock * current);
+void brush_addtolist(brush_t * b, * lst);
+void brush_create(brush_t * b);
+
+brush_t *previous, *owner, *next;
+
+enum brushmode{
+ TBrushFree = 0,
+ TBrushLock = 1,
+ TBrushPrint = 3,
+ TBrushPrimit = 4
+};
+
 };
