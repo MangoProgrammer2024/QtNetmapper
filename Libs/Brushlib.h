e593dcb3e19ffbf5698ff9a0d2f0c0cb44c112a5
BrushDeveloper <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-10-17 09:30:37 -0500

    Update Brushlib.h

--- Libs/Brushlib.h
+++ Libs/Brushlib.h
@@ -13,4 +13,14 @@ typedef class brush_t{
 public:
 const char * brush_name(const brush_t * b)const;
 
+brush_t * brushnode;
+
+static int bNumberid;
+
+enum brushtype{
+ TBspBrush = 0,
+ TCsgBrush = 1,
+ THollowBrush = 2
+};
+
 };
